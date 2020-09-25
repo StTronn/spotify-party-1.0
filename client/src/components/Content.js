dd920fa3562906ec66130a19d9cc72111174e449
rishav <rishav.21m@gmail.com>
2020-09-25 23:18:32 +0530

    added overflow in content

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -3,15 +3,9 @@ import styled from "styled-components";
 import Heading from "./Heading";
 import Card from "./Card";
 
-const TrackCointainer = styled.div`
-  display: grid;
-  grid-row-gap: 20px;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-`;
-
 function Content({ playbackObj, creatorName, roomId }) {
   return (
-    <div className="max-h-full overflow-y-auto px-12 py-6 mt-10">
+    <div className="max-h-full overflow-y-auto px-12 py-6 mt-10 overflow-auto">
       <Heading
         title="See What Other's Are Jamming To"
         subtitle={creatorName + "'s room"}
